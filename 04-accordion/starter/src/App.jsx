4ad5e30ff1fd5afd415085926a7046fe00e45425
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2024-03-05 16:31:46 +0000

    progress

--- 04-accordion/starter/src/App.jsx
+++ 04-accordion/starter/src/App.jsx
@@ -4,6 +4,11 @@ import Questions from "./Questions.jsx";
 
 const App = () => {
 	const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null)
+
+  const toggleQuestion = (id) => {
+    console.log(id);
+  }
 	return (
 		<main>
 			<Questions questions={questions} />
