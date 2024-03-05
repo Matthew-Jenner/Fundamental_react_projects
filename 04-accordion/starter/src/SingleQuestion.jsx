de885b0778b0efca84113667d967abe78eb431ff
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2024-03-05 10:36:15 +0000

    fixed importing issue

--- 04-accordion/starter/src/SingleQuestion.jsx
+++ 04-accordion/starter/src/SingleQuestion.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
+import React from "react";
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-const SingleQuestion = () => {
-    const [showInfo, setShowInfo] = useState(false)
-  return (
-    <article className="question">
-        <header>
-            <h5>{title}</h5>
-        </header>
-        <p>{info}</p>
-    </article>
-  )
-}
+const SingleQuestion = ({title, info}) => {
+	const [showInfo, setShowInfo] = useState(false);
+	return (
+		<article className="question">
+			<header>
+				<h5>{title}</h5>
+			</header>
+			<p>{info}</p>
+		</article>
+	);
+};
 
-export default SingleQuestion
+export default SingleQuestion;
