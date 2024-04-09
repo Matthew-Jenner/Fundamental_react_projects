76d04d8221ac980dfdb660d9db6d10bc5685c816
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2024-04-09 12:15:07 +0100

    unique categories with new Set

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react';
-import Title from './Title'
-import menu from './data'
-import Menu from './Menu';
+import { useState } from "react";
+import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ['all',...tempSet]
+// console.log(tempItems); same as below code
+
+const allCategories = ['all',...new Set(menu.map((item) => item.category))]
 
 const App = () => {
-  const [menuItems, setMenuItems] = useState(menu); 
-  return (<main>
-    <section className="menu">
-      <Title text= 'our menu'/>
-      <Menu items={menuItems}/>
-    </section>
-  </main>
-  )
-}
+	const [menuItems, setMenuItems] = useState(menu);
+  const [categories, newCategories] = useState(allCategories)
+  console.log(categories);
+	return (
+		<main>
+			<section className="menu">
+				<Title text="our menu" />
+				<Menu items={menuItems} />
+			</section>
+		</main>
+	);
+};
 export default App;
